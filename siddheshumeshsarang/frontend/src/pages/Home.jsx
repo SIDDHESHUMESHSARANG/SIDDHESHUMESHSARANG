8bac2cbc87125446884fa2919ebcd394651037cb
Siddhesh Umesh Sarang <siddheshsarang0811@gmail.com>
2025-10-07 17:37:23 +0530

    better desktop ui

--- siddheshumeshsarang/frontend/src/pages/Home.jsx
+++ siddheshumeshsarang/frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
       <MenuBar />
         <div>
           <div className="flex flex-col mt-15 md:mt-20">
-          <div className="">
+          <div className="md:flex md:flex-col md:justify-center md:items-center">
             <SplitText
               text={`Hi, This is Siddhesh!`}
               className="md:text-[42px] text-[40px] mt-10 ml-8"
@@ -42,17 +42,17 @@ const Home = () => {
                 className="custom-class text-[15px] md:text-[18px] ml-5 mt-3 p-2"
               >[ Developer, CS Major, a chill guy ]</GradientText>
 
-              <ShinyText text={"I'm from Mumbai,India"} disabled={false} speed={3} className='custom-class text-[20px] ml-8 mt-10 text-[#505050] md:text-[22px]' /> <br />
-              <ShinyText text={"and i code sometimes"} disabled={false} speed={3} className='custom-class text-[20px] ml-8 text-[#505050] md:text-[22px]' />
+              <ShinyText text={"I'm from Mumbai,India"} disabled={false} speed={3} className='custom-class text-[20px] ml-8 md:ml-20 mt-10 text-[#505050] md:text-[22px]' /> <br />
+              <ShinyText text={"and i code sometimes"} disabled={false} speed={3} className='custom-class text-[20px] ml-8 md:ml-20 text-[#505050] md:text-[22px]' />
 
             </div>
-              <a className="sayhibtn flex flex-row gap-1 md:w-[130px] w-[120px] text-[18px] ml-8 mt-10 p-3 mdp-5 text-white transition duration-500 ease-in-out transform active:scale-115 hover:scale-105" href="mailto:siddheshsarang0811@gmail.com">
+              <a className="sayhibtn flex flex-row gap-1 md:w-[130px] w-[120px] text-[18px] ml-8 md:ml-0 mt-10 p-3 mdp-5 text-white transition duration-500 ease-in-out transform active:scale-115 hover:scale-105" href="mailto:siddheshsarang0811@gmail.com">
                 <button className="cursor-pointer">Say Hi</button>
                 <CiMail size={25} />
               </a>
             </div>
           </div>
-        <div className="mt-40 md:ml-5">
+        <div className="mt-40 md:ml-5 md:flex md:justify-center md:items-center">
           <div className="md:flex md:gap-8">
             <img className="border border-[#404040] stats ml-5 md:ml-0 md:h-auto md:w-auto p-2 w-[90%]" src="https://leetcard.jacoblin.cool/sid-08?&theme=dark&hide_border=true" alt={<AltError/>} height='auto' width='auto' /><br />
           </div>
@@ -61,35 +61,37 @@ const Home = () => {
         <div>
           <BotpressChat />
         </div>
-        <p className="text-[25px] ml-6 mt-20">Trying to contact me?</p>
-        <p className="ml-6 text-[gray]">checkout for these</p>
-        <div className="md:flex md:flex-row md:gap-5 scrollable md:h-0 w-80 md:w-120 m-7">
-          <div className="flex mt-10 md:mt-5">
-            <SiGmail size={25} color={"gray"}/>
-            <a href="mailto:siddheshsarang0811@gmail.com" target="_blank">
-              <p id="links" className="text-[17px] text-[gray]">&nbsp;siddheshsarang0811@gmail.com</p>
-            </a>
-          </div>
-          <br /><br />
-          <div className="flex md:mt-5">
-            <FaGithub size={25} color={"gray"} />
-            <a href="https://github.com/SIDDHESHUMESHSARANG" target="_blank">
-              <p id="links" className="text-[17px] text-[gray]">&nbsp;SIDDHESHUMESHSARANG</p>
-            </a>
-          </div>
-          <br /><br />
-          <div className="flex md:mt-5">
-            <FaXTwitter size={25} color={"gray"} />
-            <a href="https://x.com/iamsid08_" target="_blank">
-              <p id="links" className="text-[17px] text-[gray]">&nbsp;iamsid08_</p>
-            </a>
-          </div>
-          <br /><br />
-          <div className="flex md:mt-5">
-            <FaInstagram size={25} color={"gray"} />
-            <a href="https://instagram.com/iamsid08" target="_blank">
-              <p id="links" className="text-[17px] text-[gray]">&nbsp;iamsid08</p>
-            </a>
+        <div className="md:flex md:flex-col md:justify-center md:items-center">
+          <p className="text-[25px] ml-6 mt-50">Trying to contact me?</p>
+          <p className="ml-6 text-[gray]">checkout for these</p>
+          <div className="md:flex md:flex-row md:gap-5 scrollable md:h-0 w-80 md:w-235 m-7">
+            <div className="flex mt-0 md:mt-5">
+              <SiGmail size={25} color={"gray"} />
+              <a href="mailto:siddheshsarang0811@gmail.com" target="_blank">
+                <p id="links" className="text-[17px] text-[gray]">&nbsp;siddheshsarang0811@gmail.com</p>
+              </a>
+            </div>
+            <br /><br />
+            <div className="flex md:mt-5">
+              <FaGithub size={25} color={"gray"} />
+              <a href="https://github.com/SIDDHESHUMESHSARANG" target="_blank">
+                <p id="links" className="text-[17px] text-[gray]">&nbsp;SIDDHESHUMESHSARANG</p>
+              </a>
+            </div>
+            <br /><br />
+            <div className="flex md:mt-5">
+              <FaXTwitter size={25} color={"gray"} />
+              <a href="https://x.com/iamsid08_" target="_blank">
+                <p id="links" className="text-[17px] text-[gray]">&nbsp;iamsid08_</p>
+              </a>
+            </div>
+            <br /><br />
+            <div className="flex md:mt-5">
+              <FaInstagram size={25} color={"gray"} />
+              <a href="https://instagram.com/iamsid08" target="_blank">
+                <p id="links" className="text-[17px] text-[gray]">&nbsp;iamsid08</p>
+              </a>
+            </div>
           </div>
         </div>
         <br />
