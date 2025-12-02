d81e6288db9fcda274e69d74db6ce2cd688ef3a2
Siddhesh Umesh Sarang <siddheshsarang0811@gmail.com>
2025-12-02 10:46:37 +0530

    Update Navbar.jsx

--- siddheshumeshsarang/frontend/src/components/Navbar.jsx
+++ siddheshumeshsarang/frontend/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         }`;
 
     return (
-        <div className='bg-black rounded-3xl'>
+        <div className='bg-black rounded-3xl md:w-[500px] w-auto'>
             <div className='flex md:text-[20px] p-1 gap-0 md:pl-0'>
                 <Link
                     to='/'
@@ -66,4 +66,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
