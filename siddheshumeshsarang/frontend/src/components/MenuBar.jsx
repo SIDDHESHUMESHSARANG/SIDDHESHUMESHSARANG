d133353d1977c2c0c6dbfd6f6881ef120b9ef3b5
Siddhesh Umesh Sarang <chmcs2026.siddhesh.sarang@gmail.com>
2026-03-13 17:03:00 +0530

    Website revamp using Next.js

--- siddheshumeshsarang/frontend/src/components/MenuBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Header from './Header'
-import Navbar from './Navbar'
-export const MenuBar = () => {
-  return (
-    <div className=''><Header /><div className='md:hidden'><Navbar /></div></div>
-  )
-}
--- siddheshumeshsarang/frontend/src/pages/blog-source/fallbacks/BlogFallback.jsx
+++
@@ -1,9 +0,0 @@
-const BlogFallback = () => {
-  return (
-    <div className="animate-pulse">
-        <div className='h-80 w-auto bg-[#404040] md:mt-0 flex items-center justify-center'><p className='text-[15px] text-[#fff]'>loading latest posts</p></div>
-    </div>
-  );
-}
-
-export default BlogFallback;
