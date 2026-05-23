f716708c56bf0f0d3a344564bab3118dd1d61718
Siddhesh Umesh Sarang <siddheshsarang0811@gmail.com>
2026-05-23 09:51:23 +0530

    Update page.js

--- sid/src/app/@about/page.js
+++ sid/src/app/@about/page.js
@@ -97,9 +97,10 @@ const AboutPage = () => {
                   <p className="mt-2 text-lg md:text-xl font-semibold">
                     {h.hint}
                   </p>
-  								</div> 
- 							))} 
-          </section>
+  				</div>
+</div>
+ ))} 
+</section>
 
           <section className="grid grid-cols-1 lg:grid-cols-5 gap-4">
             <div className="lg:col-span-3 rounded-3xl border border-primary/20 bg-base-300 shadow-2xl">
